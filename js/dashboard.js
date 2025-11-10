c83b22b7e8b512b65c7759554280201aef1e2653
taisumbelina <umbelina.uni@gmail.com>
2025-11-10 20:52:34 -0300

    dashboard 2

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,14 +1,10 @@
-// js/dashboard.js
 const greet = document.getElementById('greet');
 const logout = document.getElementById('logout');
 
 const ra = sessionStorage.getItem('loggedRA');
 if (!ra) {
-  // no session -> redirect to login
   window.location.href = 'login.html';
 } else {
-  // Display RA or a nicer name. The example image shows "Emilly" — you can adapt.
-  // For demo we show RA and a friendly name (if you want to change, edit below)
   greet.innerHTML = `Olá, ${ra}!`;
 }
 
